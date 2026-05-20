7018ad83b369ac2026a48155230707aff491d813
zakharovda95 <zakharovda95@gmail.com>
2026-05-20 22:49:41 +0300

    chore: Базово настроить проект.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '@/app.module';
+import { DEFAULT_PORT } from '@/libs/constants/app.constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = process.env.PORT || DEFAULT_PORT;
+  await app.listen(port, () => console.log(`Listening on ${port}`));
 }
-bootstrap();
+
+void bootstrap();
