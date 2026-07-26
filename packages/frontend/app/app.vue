1623d8ca1642d96fb6c343800e09e4071853a522
zakharovda95 <zakharovda95@gmail.com>
2026-07-26 14:09:11 +0300

    chore: Заменить компонент модального окна на библиотеку.

--- packages/frontend/app/app.vue
+++ packages/frontend/app/app.vue
@@ -4,6 +4,10 @@
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
+
+  <ModalsContainer />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ModalsContainer } from 'vue-final-modal';
+</script>
--- packages/frontend/app/components/ui/modals/UIModal.vue
+++ packages/frontend/app/components/ui/modals/UIModal.vue
@@ -1,51 +1,64 @@
 <template>
-  <Teleport to="#teleports">
+  <VueFinalModal
+    v-model="isOpen"
+    :teleport-to="options.teleportTo"
+    :display-directive="options.displayDirective"
+    :hide-overlay="options.hideOverlay"
+    :overlay-transition="options.overlayTransition"
+    :content-transition="options.contentTransition"
+    :click-to-close="options.clickToClose"
+    :esc-to-close="options.escToClose"
+    :background="options.background"
+    :lock-scroll="options.lockScroll"
+    :reserve-scroll-bar-gap="options.reserveScrollBarGap"
+    :swipe-to-close="options.swipeToClose"
+    class="flex items-center justify-center"
+    content-class=""
+  >
     <div
-      v-if="isOpen"
       class="size-screen bg-light-black-opacity absolute inset-0 flex items-center justify-center overflow-hidden p-12"
-      @click="closeModal(closeOnOverlay)"
     >
-      <Transition name="modal-showed" mode="out-in">
-        <div v-if="isOpen" class="bg-light-base rounded-12 relative size-fit max-h-[80%] max-w-[80%] p-12">
-          <header class="mb-12 flex items-center justify-between gap-8">
-            <div v-if="title || slots.header" class="flex-1 font-medium">
-              <slot name="header">
-                {{ title }}
-              </slot>
-            </div>
-            <UIIconButton
-              v-if="!hideCloseButton"
-              :background-color="EColor.LIGHT_200"
-              :color="EColor.LIGHT_800"
-              class="absolute right-0 bottom-[calc(100%+8px)]"
-              icon="close-line"
-              @click:button.stop="closeModal()"
-            />
-          </header>
-
-          <div class="h-fit w-auto">
-            <slot />
+      <div class="bg-light-base rounded-12 relative size-fit max-h-[80%] max-w-[80%] p-12">
+        <header class="mb-12 flex items-center justify-between gap-8">
+          <div v-if="title || slots.header" class="flex-1 font-medium">
+            <slot name="header">
+              {{ title }}
+            </slot>
           </div>
+          <UIIconButton
+            v-if="!hideCloseButton"
+            :background-color="EColor.LIGHT_200"
+            :color="EColor.LIGHT_800"
+            class="absolute right-0 bottom-[calc(100%+8px)]"
+            icon="close-line"
+            @click:button="isOpen = false"
+          />
+        </header>
 
-          <footer v-if="slots.footer" class="mt-12">
-            <slot name="footer" />
-          </footer>
+        <div class="h-fit w-auto">
+          <slot />
         </div>
-      </Transition>
+
+        <footer v-if="slots.footer" class="mt-12">
+          <slot name="footer" />
+        </footer>
+      </div>
     </div>
-  </Teleport>
+  </VueFinalModal>
 </template>
 
 <script setup lang="ts">
+import { VueFinalModal } from 'vue-final-modal';
 import { EColor } from '@kanban-board/common';
 
 import UIIconButton from '~/components/ui/buttons/UIIconButton.vue';
 
 const slots = defineSlots();
 
+const isOpen = defineModel<boolean>('isOpen', { required: true });
+
 const props = withDefaults(
   defineProps<{
-    isOpen: boolean;
     title?: string | null;
     closeOnOverlay?: boolean;
     hideCloseButton?: boolean;
@@ -57,10 +70,17 @@ const props = withDefaults(
   },
 );
 
-const emit = defineEmits<{ 'update:is-open': [value: boolean] }>();
-
-const closeModal = (canClose: boolean = true): void => {
-  if (!canClose) return;
-  emit('update:is-open', !props.isOpen);
-};
+const options = ref({
+  teleportTo: '#teleports',
+  displayDirective: 'if' as const,
+  hideOverlay: false,
+  overlayTransition: 'vfm-fade',
+  contentTransition: 'vfm-fade',
+  clickToClose: props.closeOnOverlay && !props.hideCloseButton,
+  escToClose: props.closeOnOverlay && !props.hideCloseButton,
+  background: 'non-interactive' as const,
+  lockScroll: true,
+  reserveScrollBarGap: true,
+  swipeToClose: 'none' as const,
+});
 </script>
