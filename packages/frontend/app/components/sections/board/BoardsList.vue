22cb71b56ac922b39ef939cf5f42ac3f746e356d
Дмитрий Захаров <zakharovda95@gmail.com>
2026-08-28 17:59:44 +0300

    feat: добавить DnD перетаскивание досок.

--- packages/frontend/app/components/sections/board/BoardsList.vue
+++ packages/frontend/app/components/sections/board/BoardsList.vue
@@ -3,35 +3,45 @@
     <UILoader v-if="boardsStore.isLoadingBoards" full />
     <template v-else>
       <h4 class="font-medium">Мои доски</h4>
-      <nav
+      <draggable
         v-if="!boardsStore.isLoadingBoards && boardsStore.boards?.length"
+        :model-value="boardsStore.boards"
         class="hide-scrollbar flex h-fit w-full flex-col gap-8 overflow-auto"
+        item-key="order"
+        group="border-list"
+        tag="nav"
+        ghost-class="drag-ghost"
+        :disabled="boardsStore.boards.length <= 1"
+        :animation="200"
+        @update:model-value="moveBoards"
+        @move="onMove"
+        @change="onChanged"
       >
-        <UILink
-          v-for="board in boardsStore.boards"
-          :key="board.id"
-          :to="`/boards/${board.id}`"
-          :hoverable="false"
-          class="border-light-200 rounded-8 bg-light-base flex h-60 w-full flex-1 shrink-0 cursor-pointer items-center justify-between gap-8 border p-8 duration-300 select-none"
-          :class="{ 'border-green!': board.id === boardId }"
-          @click="onClickBoard(board.id)"
-        >
-          <div class="w-[calc(100%-32px)] text-left">
-            <p class="text-14 block overflow-hidden leading-18 font-medium text-ellipsis whitespace-nowrap">
-              {{ board.title }}
-            </p>
-            <p class="text-12 block overflow-hidden leading-16 font-light text-ellipsis whitespace-nowrap">
-              {{ board.description }}
-            </p>
-          </div>
+        <template #item="{ element: board }">
+          <UILink
+            :to="`/boards/${board.id}`"
+            :hoverable="false"
+            class="border-light-200 rounded-8 bg-light-base flex h-60 w-full flex-1 shrink-0 cursor-pointer items-center justify-between gap-8 border p-8 duration-300 select-none"
+            :class="{ 'border-green!': board.id === boardId }"
+            @click="onClickBoard(board.id)"
+          >
+            <div class="w-[calc(100%-32px)] text-left">
+              <p class="text-14 block overflow-hidden leading-18 font-medium text-ellipsis whitespace-nowrap">
+                {{ board.title }}
+              </p>
+              <p class="text-12 block overflow-hidden leading-16 font-light text-ellipsis whitespace-nowrap">
+                {{ board.description }}
+              </p>
+            </div>
 
-          <BoardActionsButtons
-            :board="board"
-            @update:board="boardsStore.updateBoard"
-            @delete:board="boardsStore.deleteBoard"
-          />
-        </UILink>
-      </nav>
+            <BoardActionsButtons
+              :board="board"
+              @update:board="boardsStore.updateBoard"
+              @delete:board="boardsStore.deleteBoard"
+            />
+          </UILink>
+        </template>
+      </draggable>
       <div v-else class="text-12 p-8">
         <p>Вы еще не добавили ни одной доски.</p>
         <p>Добавьте новую доску для начала работы.</p>
@@ -47,7 +57,8 @@
 </template>
 
 <script setup lang="ts">
-import { BOARDS_MAX_COUNT } from '@kanban-board/common';
+import draggable from 'vuedraggable';
+import { BOARDS_MAX_COUNT, type TBoardBase } from '@kanban-board/common';
 
 import { useBoardsStore } from '~/stores/boards.store.ts';
 
@@ -69,5 +80,18 @@ const isTooMuchBoards = computed(() => boardsStore.boards.length >= BOARDS_MAX_C
 const onClickBoard = (id: number) => {
   if (id === boardId.value) return;
   emit('click:board');
+};
+
+const onMove = (event: object) => {
+  console.log('onMove', event);
+};
+
+const onChanged = async (updatedItems: object) => {
+  console.log('onChanged', updatedItems);
+};
+
+const moveBoards = (boards: TBoardBase[]) => {
+  console.log('update:modelValue', boards);
+  boardsStore.boards = [...boards];
 };
 </script>
--- packages/frontend/app/pages/about.vue
+++ packages/frontend/app/pages/about.vue
@@ -1,7 +1,53 @@
 <template>
-  <div class="flex size-full items-center justify-center">
+  <div class="flex size-full flex-col items-center justify-center gap-24">
     <span class="text-16 font-medium">Страница в разработке</span>
+
+    <draggable :model-value="items" v-bind="dragOptions" @move="logChange" @update:model-value="updateItems">
+      <template #item="{ element: item }">
+        <div class="rounded-6 bg-light-base h-60 cursor-move p-12" :data-order="`order-${item.order}`">
+          {{ item.message }}
+        </div>
+      </template>
+    </draggable>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import draggable from 'vuedraggable';
+import { sleep } from '@kanban-board/common';
+
+const dragOptions = ref({
+  class: 'flex flex-col gap-12',
+  animation: 200,
+  itemKey: 'order',
+  group: 'test',
+  disabled: false,
+  ghostClass: 'border border-green',
+  tag: 'ul',
+  componentData: {
+    tag: 'li',
+  },
+});
+
+type TItem = { order: number; message: string };
+
+const items = ref<TItem[]>([
+  { order: 1, message: 'Test One' },
+  { order: 2, message: 'Test Two' },
+  { order: 3, message: 'Test Three' },
+  { order: 4, message: 'Test Four' },
+  { order: 5, message: 'Test Five' },
+]);
+
+const movedItem = ref<object | null>(null);
+const logChange = (ev: object, data: object) => {
+  console.log('movedItem', ev, data);
+  movedItem.value = ev;
+};
+
+const updateItems = async (updatedItems: TItem[]) => {
+  console.log('updateItem');
+  await sleep(1000);
+  items.value = [...updatedItems];
+};
+</script>
